40f67cf437ef109cc8c6c4bccd46ad25f19df233
JacobRB05 <j.rojasb234@uniandes.edu.co>
2025-11-21 08:02:41 -0500

    más cambios

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ def translate_english(event):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+    output_div.innerText = arrr.translate(english) + "I love you mateeee"
     
 def translate_español(event):
     input_text = document.querySelector("#español")
     english = input_text.value
     output_div = document.querySelector("#output2")
-    output_div.innerText = arrr.translate(english)
+    res = arrr.translate(english) + "Te amo mateeee"
+    output_div.innerText = res
