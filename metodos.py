e46737af0803eefe51704ada442d7ba691a555c7
JacobRB05 <j.rojasb234@uniandes.edu.co>
2025-11-23 19:56:03 -0500

    clarificación

--- metodos.py
+++ metodos.py
@@ -9,33 +9,56 @@ from clases import MaquinaCorte
 
 
 mapaProcesos = {}
+mapaMaquinas = {}
+mapaPiezas = {}
 
 
 
 
-
-
-def agregarNuevoProceso(nombreProceso:str, DiametroInicialPieza:int, DiametroFinalPieza:int,longitudCorte:int, maquina: MaquinaCorte, pieza:Pieza):
-    proceso = ProcesoCorte(nombreProceso, DiametroInicialPieza, DiametroFinalPieza,longitudCorte, maquina, pieza)
+# Funciones para manejar los procesos de corte
+def agregarNuevoProceso(nombreProceso:str, DiametroInicialPieza:int, DiametroFinalPieza:int,longitudCorte:int, nombreMaquina: str, nombrePieza:str):
+    pieza = mapaPiezas.get(nombrePieza)
+    proceso = ProcesoCorte(nombreProceso, DiametroInicialPieza, DiametroFinalPieza,longitudCorte, mapaMaquinas.get(nombreMaquina), pieza)
     mapaProcesos[nombreProceso] = proceso
 
 
 def agregarPieza(nombrePieza:str,material:str ):
     pieza = Pieza(nombrePieza, material)
+    mapaPiezas[nombrePieza] = pieza
     return pieza
 
+def agregarMaquinaCorte(nombreMaquina:str):
+    maquina = MaquinaCorte(nombreMaquina)
+    mapaMaquinas[nombreMaquina] = maquina
+    return maquina
 
+def agregarMaterialMaquina(nombreMaquina:str, material:str, velocidadCorte:int, avancePorRevolucion:int,fuerzaCorte:int):
+    maquina = mapaMaquinas.get(nombreMaquina)
+    if maquina:
+        maquina.agregarMaterial(material, velocidadCorte, avancePorRevolucion,fuerzaCorte)
+    else:
+        print(f"La maquina {nombreMaquina} no existe en el mapa de maquinas.")
+
+
+#inputs
 
 diametroInicial = 50
 diametroFinal = 30
 longitudCorte = 100
+nombreMaquina = "Torno CNC"
 
-maquina = MaquinaCorte("Torno CNC")
-maquina.agregarMaterial("Acero", 150, 0.2, 500)
-maquina.agregarMaterial("Aluminio", 300, 0.3, 200)
 
-pieza = agregarPieza("Eje", "Acero")
-agregarNuevoProceso("Corte Eje", diametroInicial, diametroFinal,longitudCorte,maquina, pieza)
+# Crear maquina, agregar materiales, crear pieza y proceso
+
+agregarMaquinaCorte(nombreMaquina)
+agregarMaterialMaquina(nombreMaquina, "Acero", 150, 0.2, 500)
+agregarMaterialMaquina(nombreMaquina,"Aluminio", 300, 0.3, 200)
+agregarPieza("Eje", "Acero")
+agregarNuevoProceso("Corte Eje", diametroInicial, diametroFinal,longitudCorte,nombreMaquina, "Eje")
+
+
+#outputs
+
 
 velocidadRotacion = mapaProcesos["Corte Eje"].getVelocidadRotacion()
 print(f"La velocidad de rotacion es: {velocidadRotacion} RPM")
