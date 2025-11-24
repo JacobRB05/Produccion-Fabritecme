93832ce1379d7399dce585f223d7ef41f4a72dba
JacobRB05 <j.rojasb234@uniandes.edu.co>
2025-11-24 07:15:47 -0500

    desde 0

--- main1.py
+++ main1.py
@@ -4,8 +4,8 @@ from pyscript import document
 
 
 def funcion1(event):
-    input_text = document.querySelector("#button1")
-    value1 = input_text.value
+    input_nombrePieza = document.querySelector("#nombrePieza")
+    nombrePieza = input_nombrePieza.value
     output_div = document.querySelector("#output")  
-    output_div.innerHTML = f"El valor ingresado es: {int(value1)**2}"
+    output_div.innerHTML = f"El valor ingresado es: {nombrePieza}"
     
